ba4fc9f5cdc2aecc2ac876091d39646ba9247cd2
Nikhil-Dev-Hack <75464384+Nikhil-Dev-Hack@users.noreply.github.com>
2021-01-08 15:21:33 +0530

    Logging Added

--- ALLMVC/Program.cs
+++ ALLMVC/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +18,16 @@ namespace ALLMVC
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+         Host.CreateDefaultBuilder(args)
+          .ConfigureWebHostDefaults(webBuilder =>
+          {
+              webBuilder.UseStartup<Startup>();
+          })
+          .ConfigureLogging(logging =>
+          {
+              logging.ClearProviders();
+              logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+              logging.AddNLog();
+          });
     }
 }
